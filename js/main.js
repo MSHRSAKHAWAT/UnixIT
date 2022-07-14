7bfb766fb00ce5b334266e39164d871e7215e202
Sakhawat Hossain <96255034+MSHRSAKHAWAT@users.noreply.github.com>
2022-07-15 00:34:12 +0600

    index img updt
    index img updt

--- js/main.js
+++ js/main.js
@@ -1,99 +1,100 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-        // sticky navbar on scroll script
-        if (this.scrollY > 20) {
-            $(".navbar").addClass("sticky");
-        } else {
-            $(".navbar").removeClass("sticky");
-        }
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
 
-        // scroll-up button show/hide script
-        if (this.scrollY > 500) {
-            $(".scroll-up-btn").addClass("show");
-        } else {
-            $(".scroll-up-btn").removeClass("show");
-        }
-    });
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
 
-    // slide-up script
-    $(".scroll-up-btn").click(function() {
-        $("html").animate({ scrollTop: 0 });
-        // removing smooth scroll on slide-up button click
-        $("html").css("scrollBehavior", "auto");
-    });
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
 
-    $(".navbar .menu li a").click(function() {
-        // applying again smooth scroll on menu items click
-        $("html").css("scrollBehavior", "smooth");
-    });
+  $(".navbar .menu li a").click(function () {
+    // applying again smooth scroll on menu items click
+    $("html").css("scrollBehavior", "smooth");
+  });
 
-    // toggle menu/navbar script
-    $(".menu-btn").click(function() {
-        $(".navbar .menu").toggleClass("active");
-        $(".menu-btn i").toggleClass("active");
-    });
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true,
-    });
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 
-    var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true,
-    });
+  var typed = new Typed(".typing-2", {
+    strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 });
 
 /* slick carousel */
 
 $(".slider").slick({
-    // dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    prevArrow: '<span class="priv_arrow"><i class="fa fa-angle-right"></i></span>',
-    nextArrow: '<span class="next_arrow"><i class="fa fa-angle-left"></i></span>',
-    // arrows: true,
+  // dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  prevArrow: '<span class="priv_arrow"><i class="fa fa-angle-left"></i></span>',
+  nextArrow: '<span class="next_arrow"><i class="fa fa-angle-right"></i></span>',
+  // arrows: true,
 
-    responsive: [{
-            breakpoint: 1155,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-                infinite: true,
-                // dots: true,
-                arrows: true,
-            },
-        },
+  responsive: [
+    {
+      breakpoint: 1155,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        // dots: true,
+        arrows: true,
+      },
+    },
 
-        {
-            breakpoint: 1000,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-                infinite: true,
-                // dots: true,
-                arrows: false,
-            },
-        },
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        // dots: true,
+        arrows: false,
+      },
+    },
 
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                // dots: true,
-                arrows: false,
-            },
-        },
-    ],
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        // dots: true,
+        arrows: false,
+      },
+    },
+  ],
 });
